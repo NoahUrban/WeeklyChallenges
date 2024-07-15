2d3076fed8914cd4c455f34b2957d8d0e63212b8
“Noah <urbannoah@yahoo.com>
2024-07-15 09:02:40 -0400

    week 6

--- ChallengesWithTestsMark8/ChallengesSet06.cs
+++ ChallengesWithTestsMark8/ChallengesSet06.cs
@@ -7,27 +7,107 @@ namespace ChallengesWithTestsMark8
     {
         public bool CollectionContainsWord(IEnumerable<string> words, string word, bool ignoreCase)
         {
-            throw new NotImplementedException();
+            if (words == null) return false;
+
+            foreach (var item in words)
+            {
+                if (item == null) break;
+
+                if (!ignoreCase)
+                {
+                    if (item.Contains(word)) return true;
+                }
+                else
+                {
+                    if (item.ToLower().Contains(word.ToLower())) return true;
+                }
+            }
+
+            return false;
         }
 
         public bool IsPrimeNumber(int num)
         {
-            throw new NotImplementedException();
+            if (num < 2) return false;
+
+            int positiveNum = Math.Abs(num);
+            for (int i = 2; i < positiveNum; i++)
+            {
+                if (positiveNum % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
 
         public int IndexOfLastUniqueLetter(string str)
         {
-            throw new NotImplementedException();
+            List<char> chars = str.ToList();
+            Dictionary<char, int> dictLetters = new Dictionary<char, int>();
+            foreach (var item in chars)
+            {
+                if (!dictLetters.ContainsKey(item))
+                {
+                    dictLetters.Add(item, 1);
+                }
+                else
+                {
+                    dictLetters[item]++;
+                }
+            }
+
+            var uniqueLetters = dictLetters.Where(x => x.Value == 1).Select(x => x.Key).ToList();
+
+            if (chars.Count < 1) return -1;
+            if (uniqueLetters.Count < 1) return -1;
+
+            char lastUniqueLetter = uniqueLetters[^1];
+
+            return str.LastIndexOf(lastUniqueLetter);
         }
 
         public int MaxConsecutiveCount(int[] numbers)
         {
-            throw new NotImplementedException();
+            int count = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int tempCount = 1;
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] == numbers[j])
+                    {
+                        tempCount++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                if (tempCount > count)
+                {
+                    count = tempCount;
+                }
+            }
+
+            return count;
         }
 
         public double[] GetEveryNthElement(List<double> elements, int n)
         {
-            throw new NotImplementedException();
+            if (elements == null) return new double[] { };
+            if (n < 1) return new double[] { };
+
+            List<double> nthDoubles = new();
+            for (int i = n - 1; i < elements.Count; i += n)
+            {
+                nthDoubles.Add(elements[i]);
+            }
+
+            return nthDoubles.ToArray();
         }
     }
 }
